0deee52ff5883d6e5c6b55d1cfb8020d75f092b9
vladimirivich.illya <vladimirivich.illya@gmail.com>
2026-01-21 13:13:41 +0000

    index. ононвлен

--- index.tsx
+++ index.tsx
@@ -2,15 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './firebase-setup'; 
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const rootElement = document.getElementById('root')!;
+
+// Define a type for our custom property on the window object
+interface CustomWindow extends Window {
+  _reactRoot?: ReactDOM.Root;
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+const customWindow = window as CustomWindow;
+
+// Use a property on the window object to store the root, as it persists across HMR updates.
+if (!customWindow._reactRoot) {
+  customWindow._reactRoot = ReactDOM.createRoot(rootElement);
+}
+
+customWindow._reactRoot.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
