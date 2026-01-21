c0aa828932343084db21475c834aa3f6cb74d973
vladimirivich.illya <vladimirivich.illya@gmail.com>
2026-01-21 13:13:46 +0000

    index.html оновлено

--- index.tsx
+++ index.tsx
@@ -1,23 +1,10 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root')!;
+const root = ReactDOM.createRoot(document.getElementById('root')!)
 
-// Define a type for our custom property on the window object
-interface CustomWindow extends Window {
-  _reactRoot?: ReactDOM.Root;
-}
-
-const customWindow = window as CustomWindow;
-
-// Use a property on the window object to store the root, as it persists across HMR updates.
-if (!customWindow._reactRoot) {
-  customWindow._reactRoot = ReactDOM.createRoot(rootElement);
-}
-
-customWindow._reactRoot.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
